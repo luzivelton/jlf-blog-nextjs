00f19c91b522ba046dce2408008bde8ccdc7e0b8
João Fernandes <joaolfern@proton.me>
2025-10-17 21:28:09 -0300

    fix: hydration error on theme button

--- src/layouts/AppLayout/components/ThemeButton.tsx
+++ src/layouts/AppLayout/components/ThemeButton.tsx
@@ -1,10 +1,19 @@
 'use client'
+
+import { useEffect, useState } from 'react'
 import { FloatingButton } from '@/components/FloatingButton/FloatingButton'
 import { LightbulbIcon, LightbulbOffIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
 export function ThemeButton() {
   const { resolvedTheme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
 
   return (
     <FloatingButton
